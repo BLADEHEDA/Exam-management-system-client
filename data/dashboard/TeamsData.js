07758fd358674d4be0b0fc99ca677def7006b9c3
BLADE-HEDA <you@example.com>
2024-07-24 23:31:20 +0100

    successfully merged the branch after resolving conflicts

--- data/dashboard/TeamsData.js
+++ data/dashboard/TeamsData.js
@@ -1,53 +1,5 @@
 export const TeamsData = [
    {
-<<<<<<< HEAD
-       id:1,
-       name : "Fozin Theohile",
-       email : "fozintheohile@gmail.com",
-       role: "Lecturer",
-       lastActivity: '3 May, 2023',
-       image:'/images/avatar/avatar-2.jpg' 
-   },
-   {
-       id:2,
-       name : "Nkemeni Valery",
-       email : "nkemenivalery@gmail.com",
-       role: "Lecturer",
-       lastActivity: 'Today',
-       image:'/images/avatar/avatar-1.jpg' 
-   },
-   {
-       id:3,
-       name : "Emmanuel Banga",
-       email : "emmanuel@example.com",
-       role: "Lecturer",
-       lastActivity: 'Yesterday',
-       image:'/images/avatar/avatar-3.jpg' 
-   },
-   {
-       id:4,
-       name : "Evelyne Nkeng",
-       email : "evelyne@example.com",
-       role: "Account Manager",
-       lastActivity: '3 May, 2023',
-       image:'/images/avatar/avatar-4.jpg' 
-   },
-   {
-       id:5,
-       name : "Mireille Ndzana",
-       email : "mireille@example.com",
-       role: "Digital Marketer",
-       lastActivity: '3 May, 2023',
-       image:'/images/avatar/avatar-5.jpg' 
-   },
-   {
-       id:6,
-       name : "Lucienne Tabe",
-       email : "lucienne@example.com",
-       role: "Front End Developer",
-       lastActivity: '3 May, 2023',
-       image:'/images/avatar/avatar-6.jpg' 
-=======
       id: 2,
       name: "Nkemeni Valery",
       email: "nkemenivalery@gmail.com",
@@ -86,7 +38,6 @@ export const TeamsData = [
       role: "Lecturer",
       lastActivity: 'N/A',
       image: '/images/avatar/avatar-6.jpg' 
->>>>>>> 3a909fb (modify this to have the form work properly)
    }
 ];
 
